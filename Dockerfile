d627d130d816cb970f65317c80e63cb72b0b4497
Marco Secreto <msegreto@miceover.com>
2014-04-09 23:10:32 -0700

    Added conf to app, reload to Dockerfile
    The reload should probably not be on production. To help this, it might work
    to have two separate dockerfiles, dev and prod. The prod one will have the
    regular go command and not get the packages. Another way would be to have the
    docker run command on prod pass in a different entrypoint or cmd.
    
    TODO:
    - Change Dockerfile to correctly use ENTRYPOINT and CMD together
    - App file is a mess

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,19 @@ MAINTAINER Marco Secret, msegreto@miceover.com
 ENV DEBIAN_FRONTEND noninteractive:w
 ENV GOPATH /srv/go
 ENV APP_PATH $GOPATH/src/github.com/msecret/trashsifter-api
+ENV PATH /srv/go/bin:$PATH
 
 ADD . $APP_PATH
 ## WORKDIR doesn't not expand env vars
 ## see https://github.com/dotcloud/docker/issues/2637 
 WORKDIR /srv/go/src/github.com/msecret/trashsifter-api
 
+VOLUME ["/srv/go/src/github.com/msecret/trashsifter-api"]
+
+RUN go get github.com/githubnemo/CompileDaemon
 RUN go get
 RUN go build
 
 EXPOSE 80
 
-ENTRYPOINT ["./trashsifter-api"]
+CMD ["CompileDaemon", "-directory=/srv/go/src/github.com/msecret/trashsifter-api", "-command=/srv/go/src/github.com/msecret/trashsifter-api/trashsifter-api"]
--- app.go
+++ app.go
@@ -1,20 +1,55 @@
 package main
 
 import (
-	"github.com/ant0ine/go-json-rest/rest"
+	"fmt"
+
 	"net/http"
+
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/kelseyhightower/envconfig"
 )
 
-type Message struct {
-	Body string
+type AppConfSpec struct {
+	Port string
+	Host string
+}
+
+type DbConfSpec struct {
+	User string
+	Pass string
+	Host string
+	Port uint
+	Db   string
+}
+
+type StatusMessage struct {
+	DbConf  string
+	AppConf string
+}
+
+func processEnvConf(appConf *AppConfSpec, dbConf *DbConfSpec) {
+	err := envconfig.Process("app", appConf)
+	if err != nil {
+		panic(err)
+	}
+	envconfig.Process("db_env", &dbConf)
 }
 
 func main() {
+	var (
+		appConf AppConfSpec
+		dbConf  DbConfSpec
+	)
+
+	processEnvConf(&appConf, &dbConf)
+	fmt.Print("adsf")
+
 	handler := rest.ResourceHandler{}
 	handler.SetRoutes(
-		&rest.Route{"GET", "/message", func(w rest.ResponseWriter, req *rest.Request) {
-			w.WriteJson(&Message{
-				Body: "Hello World!",
+		&rest.Route{"GET", "/st", func(w rest.ResponseWriter, req *rest.Request) {
+			w.WriteJson(&StatusMessage{
+				DbConf:  fmt.Sprintf("%+v", dbConf),
+				AppConf: fmt.Sprintf("%+v", appConf),
 			})
 		}},
 	)
